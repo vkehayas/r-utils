d5a43b21b9608a662df678871ef3a81e27963466
Vassilis Kehayas <vkehayas@gmail.com>
2018-11-28 13:51:55 +0000

    Update LoadPackages.R

--- LoadPackages.R
+++ LoadPackages.R
@@ -1,26 +1,28 @@
-LoadPackages = function(packageName, gitUser = NULL) {
-  #' Load installed package or install it from CRAN (with argument gitUser 
-  #' missing or NULL) or a GitHub user's repo if the package is missing and then 
-  #' load it. A utility function `install_github` from package `ghit` will be 
-  #' installed if not found from that or another package.
-  
-  
-  if (!require(packageName, character.only = TRUE)) {
-    # Attempts to load package. If it does not exist, the above returns TRUE
-    
-    if (!is.null(gitUser)) {
-      
-      if (!exists("install_github")) {
-        LoadPackages("ghit")
+LoadPackages = function(packageNameList, gitUser = NULL) {
+  # Load installed package or install it from CRAN (with argument gitUser
+  # missing or NULL) or a GitHub user's repo if the package is missing and then
+  # load it. A utility function `install_github` from package `remotes` will be
+  # installed if not found from that or another package.
+
+  for (iPackages in packageNameList) {
+
+    if (!require(iPackages, character.only = TRUE)) {
+      # Attempts to load package, if it does not exist the above returns TRUE
+
+      if (!is.null(gitUser)) {
+
+        if (!exists("install_github")) {
+          LoadPackages("remotes")
+        }
+        install_github(paste(gitUser, iPackages, sep = "/"))
+
+      } else {
+        install.packages(iPackages, dependencies = TRUE)
       }
-      install_github(paste(gitUser, packageName, sep = "/"))
-      
-    } else {
-      install.packages(packageName, dependencies = TRUE)
+
+      library(iPackages, character.only = TRUE)
     }
-    
-    library(packageName, character.only = TRUE)
+
   }
-  
-  
+
 }
